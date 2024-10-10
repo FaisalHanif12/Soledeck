b0f40f531de3e1f3a2000c913d86927f9e760fa2
Faisal Hanif <mehrfaisal111@example.com>
2024-10-10 15:01:09 -0700

    add the cors

--- server/index.js
+++ server/index.js
@@ -4,12 +4,23 @@ import 'dotenv/config';
 import { typeDefs } from './graphql/typeDefs.js';
 import resolvers from './graphql/resolvers/index.js';
 import connectDB from './db/connect.js';
+import cors from 'cors'; // Import CORS module
 
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+const corsOptions = {
+  origin: 'https://soledeckf.vercel.app', // Set specific origin
+  credentials: true, 
+};
+
+
+app.use(cors(corsOptions));
+
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', function (req, res) {
@@ -26,10 +37,10 @@ const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   await apolloServer.start();
-  apolloServer.applyMiddleware({ app: app });
+  apolloServer.applyMiddleware({ app, cors: corsOptions }); // Apply CORS to Apollo Server as well
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(PORT, () => console.log('Server is running'));
+    app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
   } catch (error) {
     throw new Error(error);
   }
